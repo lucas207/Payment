d888efdf6971a33f1297da3f59e97fb62c55e09f
Dancrash <lucas.nota10@hotmail.com>
2021-08-19 21:38:37 -0300

    planning

---
+++ Eice.Payment.Domain/Client/Service.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eice.Payment.Domain.Client
+{
+    public class Service
+    {
+        //Validar client able to Doar Atom
+        //Validar client able to Receber Atom
+    }
+}
